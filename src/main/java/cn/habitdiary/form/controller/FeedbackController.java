88d49d1ac635926161ff2e7952d339c01b31d863
betterme <573668878@qq.com>
2018-08-06 15:19:32 +0800

    successPage

--- src/main/java/cn/habitdiary/form/controller/FeedbackController.java
+++ src/main/java/cn/habitdiary/form/controller/FeedbackController.java
@@ -3,6 +3,7 @@ package cn.habitdiary.form.controller;
 import cn.habitdiary.form.service.FeedbackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,10 +22,13 @@ public class FeedbackController {
     @PostMapping("/doCollect")
     public String doCollect(@RequestParam("itemValue") String itemValue, @RequestParam("uuid") String uuid,
                             @RequestParam("userid") Integer userid, @RequestParam("formname") String formname,
-                            @RequestParam("formid") Integer formid) {
+                            @RequestParam("formid") Integer formid,Model model) {
 
         String[] items = itemValue.split(",");
         feedbackService.fillForm(formid,uuid,userid,formname,items);
-        return "index";
+        model.addAttribute("uuid",uuid);
+        model.addAttribute("userid",userid);
+        model.addAttribute("formname",formname);
+        return "success";
     }
 }
